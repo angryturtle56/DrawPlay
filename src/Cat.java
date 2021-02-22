f03b3c160796e2374d895397fe6dd52b80b84abf
Emily Hime <emilyhime@mines.edu>
2021-02-21 19:42:32 -0700

    Emily's Commit

--- src/Cat.java
+++ src/Cat.java
@@ -1,6 +1,7 @@
 import javax.swing.JPanel;
 import java.awt.*;
 import java.awt.geom.*;
+import java.util.*;
 
 public class Cat {
 	// drawing constants are private - noone needs to know what we're doing
