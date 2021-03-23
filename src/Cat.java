4e97dafafc0c8d2035a1e3112d1214b2ce0f66c5
angryturtle56 <79273936+angryturtle56@users.noreply.github.com>
2021-03-22 21:57:46 -0600

    Initial Commit

--- src/Cat.java
+++ src/Cat.java
@@ -1,7 +1,6 @@
 import javax.swing.JPanel;
 import java.awt.*;
 import java.awt.geom.*;
-import java.util.*;
 
 public class Cat {
 	// drawing constants are private - noone needs to know what we're doing
@@ -15,8 +14,8 @@ public class Cat {
 	private static final int EYE_HEIGHT = 20;
 	private static final int EYE_WIDTH = 10;
 	// pick mouth height, width is based on head dimension
-	private static final int MOUTH_HEIGHT = 50;
-	private static final int MOUTH_WIDTH = HEAD_DIMENSION/2;
+	private static final int MOUTH_HEIGHT = 10;
+	private static final int MOUTH_WIDTH = HEAD_DIMENSION/4;
 	// mouth starts about 40% from left edge of head
 	private static final int MOUTH_X = HEAD_DIMENSION/5 * 2;
 	private static final int MOUTH_Y = HEAD_DIMENSION/5 * 3;
@@ -28,14 +27,14 @@ public class Cat {
 		int x=catX;
 		int y=catY;
 		// Draw the head
-		g2.setColor(Color.lightGray);
+		g2.setColor(Color.black);
 		g2.fillOval(x, y, HEAD_DIMENSION, HEAD_DIMENSION);
 		// Draw the eyes
-		g2.setColor(Color.cyan);
+		g2.setColor(Color.green);
 		x = catX + EYE_X; 
 		y = catY + EYE_Y;
 		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
-		x = x + EYE_SEPARATION + EYE_SEPARATION;
+		x += EYE_SEPARATION;
 		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
 		// Draw the mouth
 		g2.setColor(Color.pink);
@@ -45,6 +44,6 @@ public class Cat {
 		g2.setColor(Color.black);
 		// Meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
-		g2.drawString("Bark", catX, catY+HEAD_DIMENSION+10);	
+		g2.drawString("Meow", catX, catY+HEAD_DIMENSION+10);	
 	}
 }
